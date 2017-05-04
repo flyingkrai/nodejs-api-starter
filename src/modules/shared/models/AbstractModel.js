50cadf13efdff8b629af3b29213623c985441283
Davi Alves <flyingkrai@gmail.com>
2017-05-04 15:21:09 -0300

    working cache logic

--- src/modules/shared/models/AbstractModel.js
+++ src/modules/shared/models/AbstractModel.js
@@ -1,60 +1,12 @@
 /* @flow */
 
-import DataLoader from 'dataloader';
-import moment from 'moment';
-import config from 'config';
-
 import db from '../../../db';
-import redis from '../../../redis';
-
-const loaders = {};
-const lastFetchTimes = {};
-
-const createCacheArray = (tableName: string, item: Object): Array =>
-  [`${tableName}:${item.id}`, JSON.stringify(item)];
-
-const setCacheItem = (tableName: string, item: Object): Promise =>
-  redis.msetAsync(createCacheArray(tableName, item));
-
-const setCacheItemAsync = (tableName: string, item: Object) => {
-  setCacheItem(tableName, item)
-    .then(() => console.log(`----> cache refreshed form ${tableName}:${item.id}`));
-};
-
-async function fetchItems(tableName, Model) {
-  lastFetchTimes[tableName] = moment();
-  const items = await Model.all();
-  await redis.msetAsync(items
-    .reduce(
-      (list, item) => list.concat(createCacheArray(tableName, item)), []));
-
-  return items;
-}
-
-function fetchItemsAsync(tableName, Model) {
-  fetchItems(tableName, Model)
-    .then(() => console.log(`----> cache refreshed for ${tableName}`));
-}
-
-function setupLoader(tableName, Model) {
-  loaders[tableName] = new DataLoader(keys => Promise.resolve()
-    .then(() => lastFetchTimes[tableName] ? null : fetchItems(tableName, Model)) // eslint-disable-line
-    .then(() => redis.mgetAsync(keys.map(x => `${tableName}:${x}`)))
-    .then(data => data.map((x, i) => {
-      if (x) return Object.assign(new Model(), x);
-
-      throw new Error(`Cannot find an item with ID ${keys[i]}`);
-    })));
-}
+import Cache from './Cache';
 
 export default class AbstractModel {
 
   static TABLE;
   static ID_KEY;
-
-  constructor() {
-    setupLoader(this.TABLE, this);
-  }
 
   /**
    * Loads given keys
@@ -63,7 +15,7 @@ export default class AbstractModel {
    * @returns {Array}
    */
   static load(keys) {
-    return loaders[this.TABLE].load(keys);
+    return Cache.getLoader(this.TABLE, this).load(keys);
   }
 
   /**
@@ -71,24 +23,11 @@ export default class AbstractModel {
    * @returns {Promise.<void>}
    */
   static async find() {
-    const keys = await redis.keysAsync(`${this.TABLE}:*`);
-    const data = keys.length ?
-      (await redis.mgetAsync(keys)).map((x, i) => ({ id: keys[i], ...JSON.parse(x) })) :
-      (await fetchItems(this.TABLE, this));
-
-    // Update cache in the background if it's older than config
-    if (
-      !lastFetchTimes[this.TABLE]
-      || moment().diff(lastFetchTimes[this.TABLE], 'minutes') > config.get('query.cacheTimeLimit')
-    ) {
-      fetchItemsAsync(this.TABLE, this);
-    }
-
-    return data.map(x => Object.assign(new this(), x));
+    return Cache.all(this.TABLE, this);
   }
 
   static findOneById(id: string) {
-    return this.find().then(list => list.find(item => item.id === id));
+    return Cache.getLoader(this.TABLE, this).load(id);
   }
 
   static all() {
@@ -110,7 +49,7 @@ export default class AbstractModel {
       .returning('id')
       .then(id => this.findOne({ [this.ID_KEY]: id }))
       .then((item) => {
-        setCacheItemAsync(this.TABLE, item);
+        Cache.updateCacheItemAsync(this.TABLE, item);
 
         return item;
       });
@@ -125,7 +64,7 @@ export default class AbstractModel {
       .then((x) => {
         this
           .findOne(where)
-          .then(item => setCacheItemAsync(this.TABLE, item));
+          .then(item => Cache.updateCacheItem(this.TABLE, item));
 
         return x[0];
       });
---
+++ src/modules/shared/models/Cache.js
@@ -0,0 +1,129 @@
+/* @flow */
+
+import config from 'config';
+import DataLoader from 'dataloader';
+import moment from 'moment';
+
+import redis from '../../../redis';
+
+const LOADERS = {};
+const FETCH_TIMER = {};
+
+export default class Cache {
+
+  /**
+   * Creates a cache key, valey array for redis
+   *
+   * @param {String} tableName
+   * @param {Object} item
+   * @returns {[string, string]}
+   */
+  static createCacheArray(tableName: string, item: Object): Array<string> {
+    return [`${tableName}:${item.id}`, JSON.stringify(item)];
+  }
+
+  /**
+   * Update a single cache item
+   *
+   * @param {String} tableName
+   * @param {Object} item
+   * @returns {Promise}
+   */
+  static updateCacheItem(tableName: string, item: Object): Promise {
+    return redis.msetAsync(this.createCacheArray(tableName, item));
+  }
+
+  /**
+   * Update a single cache item asynchronously
+   * @param tableName
+   * @param item
+   */
+  static updateCacheItemAsync(tableName: string, item: Object) {
+    Cache.updateCacheItem(tableName, item)
+      .then(() => console.log(`----> cache refreshed form ${tableName}:${item.id}`));
+  }
+
+  /**
+   * Update cache for given table
+   *
+   * @param {String} tableName
+   * @param {Object} Model
+   * @returns {Promise}
+   */
+  static update(tableName: string, Model: Object): Promise {
+    FETCH_TIMER[tableName] = moment();
+
+    return Model.all()
+      .then(items =>
+        redis
+          .msetAsync(
+            items.reduce((list, item) =>
+              list.concat(Cache.createCacheArray(tableName, item)), []),
+          )
+          .then(() => items),
+      );
+  }
+
+  /**
+   * Update cache for given table asynchronously
+   *
+   * @param {String} tableName
+   * @param {Object} Model
+   */
+  static updateAsync(tableName: string, Model: Object) {
+    Cache.update(tableName, Model)
+      .then(() => console.log(`----> cache refreshed for ${tableName}`));
+  }
+
+  /**
+   * Checks if the cache has expired
+   *
+   * @param {String} tableName
+   * @returns {boolean}
+   */
+  static hasExpired(tableName: string): boolean {
+    console.log('expitarion check', tableName);
+    return !FETCH_TIMER[tableName]
+      || moment().diff(FETCH_TIMER[tableName], 'minutes') > config.get('query.cacheTimeLimit');
+  }
+
+  /**
+   * Retrieves the dataloader for given table
+   *
+   * @param {String} tableName
+   * @param {Object} Model
+   * @returns {Object}
+   */
+  static getLoader(tableName: string, Model): Object {
+    if (LOADERS[tableName]) return LOADERS[tableName];
+
+    LOADERS[tableName] = new DataLoader(keys => Promise.resolve()
+      .then(() => Cache.hasExpired(tableName) ? Cache.update(tableName, Model) : null) // eslint-disable-line
+      .then(() => redis.mgetAsync(keys.map(key => `${tableName}:${key}`)))
+      .then(data => data.map((item, i) => {
+        if (item) return Model.parse(JSON.parse(item));
+        throw new Error(`Cannot find an article with ID ${keys[i]}`);
+      })));
+
+    return LOADERS[tableName];
+  }
+
+  /**
+   * Loads all items from cache
+   *
+   * @param {String} tableName
+   * @param {Object} Model
+   * @returns {Promise}
+   */
+  static all = async (tableName: string, Model): Promise => {
+    const keys = await redis.keysAsync(`${tableName}:*`);
+    const data = keys.length ?
+      (await redis.mgetAsync(keys)).map(x => JSON.parse(x)) :
+      (await Cache.update(tableName, Model));
+
+    // Update cache in the background if it's older than config
+    if (Cache.hasExpired(tableName)) Cache.updateAsync(tableName, Model);
+
+    return data.map(item => Object.assign(new Model(), item));
+  };
+}
