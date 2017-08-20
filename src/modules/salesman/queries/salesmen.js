0049f62a7c08f16e199c126b1153c4abbcef8033
Davi Alves <flyingkrai@gmail.com>
2017-08-20 19:03:34 -0300

    added total count to connection queries

--- src/modules/salesman/queries/salesmen.js
+++ src/modules/salesman/queries/salesmen.js
@@ -1,9 +1,19 @@
+import { GraphQLInt } from 'graphql';
 import { connectionArgs, connectionDefinitions, connectionFromPromisedArray } from 'graphql-relay';
 
 import Salesman from '../models/Salesman';
 import SalesmanType from '../types/SalesmanType';
 
-const articles = connectionDefinitions({ name: 'Salesman', nodeType: SalesmanType });
+const articles = connectionDefinitions({
+  name: 'Salesman',
+  nodeType: SalesmanType,
+  connectionFields: {
+    totalCount: {
+      type: GraphQLInt,
+      resolve: () => Salesman.find().then(res => (Array.isArray(res) ? res : []).length),
+    },
+  },
+});
 
 export default {
   type: articles.connectionType,
--- src/modules/vehicle/queries/vehicles.js
+++ src/modules/vehicle/queries/vehicles.js
@@ -1,9 +1,19 @@
+import { GraphQLInt } from 'graphql';
 import { connectionArgs, connectionDefinitions, connectionFromPromisedArray } from 'graphql-relay';
 
 import Vehicle from '../models/Vehicle';
 import VehicleType from '../types/VehicleType';
 
-const articles = connectionDefinitions({ name: 'Vehicle', nodeType: VehicleType });
+const articles = connectionDefinitions({
+  name: 'Vehicle',
+  nodeType: VehicleType,
+  connectionFields: {
+    totalCount: {
+      type: GraphQLInt,
+      resolve: () => Vehicle.find().then(res => (Array.isArray(res) ? res : []).length),
+    },
+  },
+});
 
 export default {
   type: articles.connectionType,
