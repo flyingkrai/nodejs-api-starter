e334f21dc66422d3d87e3d2d744f5e3bc1d7c941
Davi Alves <hey@davialves.com>
2017-08-21 16:09:41 -0300

    docker fixes and package-lock

--- config/postgres-initdb.sh
+++ config/postgres-initdb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --password "$POSTGRES_PASSWORD" <<-EOSQL
   CREATE DATABASE prod;
   CREATE DATABASE dev;
   CREATE DATABASE test;
